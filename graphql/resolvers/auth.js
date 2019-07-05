03581b728fbc6676001ab01d40aed6893980baca
AntySkill <sebastian.juras@gmail.com>
2019-07-05 21:42:35 +0200

    Login authorization with jwt

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../../models/user');
-
 
 module.exports = {
   createUser: async args => {
@@ -17,12 +17,29 @@ module.exports = {
         password: hashedPassword
       });
 
-    
       const result = await user.save();
 
       return { ...result._doc, password: null, _id: result.id };
     } catch (err) {
       throw err;
     }
+  },
+  login: async ({ email, password }) => {
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      throw new Error('User does not exist!');
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      throw new Error('Password is incorrect!');
+    }
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      'somesupersecretkey',
+      {
+        expiresIn: '1h'
+      }
+    );
+    return { userId: user.id, token: token, tokenExpiration: 1 };
   }
 };
